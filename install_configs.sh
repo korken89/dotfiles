bc83bfdcd44deba5e45b793c071c17c9a3ac4fa8
Emil Fresk <emil.fresk@gmail.com>
2022-10-13 20:53:26 +0200

    More

--- install_configs.sh
+++ install_configs.sh
@@ -23,8 +23,37 @@ ln -sf "$(pwd)/helix_config.toml" ~/.config/helix/config.toml
 
 sudo ln -sf "$(pwd)/pixellock" /usr/bin/pixellock
 
+
 # Install minimal stuff
-sudo pacman -S alacritty awesome rofi nitrogen picom volumeicon helix  --needed --noconfirm
+sudo pacman -S alacritty awesome rofi nitrogen picom volumeicon helix git base-devel \
+               light maim xclip thunar firefox lxappearance network-manager-applet \
+               docker blueman wireplumber pipewire-v4l2 pipewire-pulse pipewire-jack \
+               pipewire-audio pipewire-alsa pipewire wireshark-qt \
+               rustup cargo-watch cargo-exapnd cargo-bloat \
+               --needed --noconfirm
+
+# Install yay and that stuff
+(git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --needed --noconfirm)
+
+yay -S nerf-fonts-complete noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \ 
+       ttf-font-awesome ttf-ms-fonts \
+       --needed --noconfirm
+
+# Groups
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo usermod -aG wheel $USER
+sudo usermod -aG plugdev $USER
+sudo usermod -aG wireshark $USER
+
+# Enable some services
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
+sudo systemctl enable docker
+sudo systemctl start docker
 
 # Extend .bashrc
 echo "[[ -f '$(pwd)/aliases' ]] && source $(pwd)/aliases " >> ~/.bashrc
